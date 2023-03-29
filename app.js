734b2cf0d1920ce98426d274d5947cc578b87542
t0nkisa <128639796+t0nkisa@users.noreply.github.com>
2023-03-29 12:46:08 +0200

    Added game logic

--- app.js
+++ app.js
@@ -9,3 +9,42 @@ function getComputerChoice() {
     }
     return choice;
 }
+
+function firstLetterToUpperCase(string){
+    return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    const playerSelection = firstLetterToUpperCase(playerSelection);
+    const computerSelection = firstLetterToUpperCase(computerSelection);
+    let result = "";
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            result = `You lose! ${playerSelection} loses to ${computerSelection}`;
+        } else if (computerSelection === "Scissors") {
+            result = `You win! ${playerSelection} beats ${computerSelection}`;
+        } else {
+            result = `You draw! ${playerSelection} draws to ${computerSelection}`;
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Paper") {
+            result = `You draw! ${playerSelection} draws to ${computerSelection}`;
+        } else if (computerSelection === "Scissors") {
+            result = `You lose! ${playerSelection} loses to ${computerSelection}`;
+        } else {
+            result = `You win! ${playerSelection} beats ${computerSelection}`;
+        }
+    } else {
+        if (computerSelection === "Paper") {
+            result = `You win! ${playerSelection} beats ${computerSelection}`;
+        } else if (computerSelection === "Scissors") {
+            result = `You draw! ${playerSelection} draws to ${computerSelection}`;
+        } else {
+            result = `You lose! ${playerSelection} loses to ${computerSelection}`;
+        }
+    }
+    return result;
+}
+
+const playerSelection = prompt("Your choice: Rock, paper or scissors?");
+const computerSelection = getComputerChoice();
